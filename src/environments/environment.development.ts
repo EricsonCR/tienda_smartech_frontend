7210159c698317a5fd555397334029cf84fc0d3d
EricsonCR <ericson4634@gmail.com>
2025-03-29 01:25:21 -0500

    {Component: header, banner, css responsive}, commit 27

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    // urlApi: "http://localhost:8080/api"
+    //urlApi: "http://localhost:8080/api"
     urlApi: "https://pleasant-blessing-production-1e47.up.railway.app/api"
 };
